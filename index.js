bbacdb951fd10e0d01ea4c5493b161f8049e6b9d
shukra <shukratamang.npl@gmail.com>
2024-04-25 16:02:34 +1000

    http server in node js

---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const myServer = http.createServer((req, res) => {
+  console.log(req.headers);
+  res.end('hello from server');
+});
+
+myServer.listen(8000, () => console.log('server started'));
